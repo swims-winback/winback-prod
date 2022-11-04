6caba15744333ef7cfa59fc21812518a2b967213
leaddn <leadieudonat@example.com>
2022-11-04 15:31:05 +0100

    dashboard

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -58,7 +58,6 @@ const myChart = new Chart(ctx, {
       var width = chart.width,
         height = chart.height,
         ctx = chart.ctx;
-      console.log(height);
       ctx.restore();
       var fontSize = (height / 114).toFixed(2);
       ctx.font = fontSize + "em sans-serif";
@@ -67,7 +66,6 @@ const myChart = new Chart(ctx, {
       var text = total_devices,
         textX = Math.round((width - ctx.measureText(text).width) / 2),
         textY = (height / 3)+(height / 3);
-      console.log(textY);
       ctx.fillText(text, textX, textY);
       ctx.save();
     }
@@ -78,14 +76,11 @@ const myChart = new Chart(ctx, {
 
 myChartArray = []
 for (const key in result) {
-  //const element = object[key];
   let substr_devices = total_devices - result[key];
   let versionZoneValue = document.getElementById(`count_version_${key}`);
   var type = 'ctx2';
   var type2 = 'myChart2';
-  //this[type+"_"+key] = 1000;
   this[type+"_"+key] = document.getElementById(`myChart2_${key}`).getContext('2d');
-  //this[type2+"_"+key] = new Chart(this[type+"_"+key], {
     myChartArray[key] = new Chart(this[type+"_"+key], {
           type: 'doughnut',
           data: {
@@ -93,7 +88,6 @@ for (const key in result) {
               datasets: [{
                 label: `${key}`,
                 data: [result[key], versionZoneValue.textContent],
-                //data: [versionZoneValue.textContent],
                 backgroundColor: [
                   'white',
                   'rgba(54, 162, 235, 0.2)'
@@ -164,28 +158,27 @@ function removeData(chart) {
 //Listen for the select options
 // when one option is selected, search all devices that corresponds to device family + version
 // get the total number of devices
-let select_options = document.getElementsByClassName('select-version');
-for (let select of select_options) {
-  $(document).ready(function() {
-    $(select).click(() => {
-      let version = select.value;
-      //let deviceFamily = '1';
-      let deviceFamily = $(select).data("devicefamily");
-      let versionZone = document.getElementById(`count_version_${deviceFamily}`);
-      $.ajax({    
-        type: "GET", 
-        //url: `/isactive/${deviceId}`,
-        url: `/version/${deviceFamily}/${version}/`,
-        dataType: "html",                  
-        success: function (data) { 
-          versionZone.innerHTML = data;
-          myChartArray[deviceFamily].data.datasets[0].data[0] = result[deviceFamily]-data;
-          myChartArray[deviceFamily].data.datasets[0].data[1] = data;
-          myChartArray[deviceFamily].update();
-        }
-      });
-    });
-  });
-}
 
+$(document).ready(function() {
+  for (let fam in family_array) {
+    console.log(family_array[fam]);
+    $(`#dashSelect_${family_array[fam]}`).on('change', () => {
+      let version = $( `#dashSelect_${family_array[fam]} option:selected` ).val();
+        let deviceFamily = $( `#dashSelect_${family_array[fam]} option:selected` ).data("devicefamily");
+        let versionZone = document.getElementById(`count_version_${deviceFamily}`);
+        $.ajax({    
+          type: "GET",
+          url: `/version/${deviceFamily}/${version}/`,
+          dataType: "html",                  
+          success: function (data) { 
+            versionZone.innerHTML = data;
+            myChartArray[deviceFamily].data.datasets[0].data[0] = result[deviceFamily]-data;
+            myChartArray[deviceFamily].data.datasets[0].data[1] = data;
+            myChartArray[deviceFamily].update();
+          }
+        });
+      });
+
+  }
+});
 
