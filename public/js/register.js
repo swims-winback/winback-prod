ebb109d6aaa9afacff0e65f1f42f4f8cd6d64a9a
leaddn <leadieudonat@example.com>
2023-02-16 14:08:24 +0100

    update register

--- public/js/register.js
+++ public/js/register.js
@@ -43,3 +43,42 @@ for (let i = 0; i < eyeIcon.length; i++) {
     togglePassword(eyeIcon.item(i), passwordField)
   }
 }
+
+function checkPasswordStrength() {
+	var number = /([0-9])/;
+	var alphabets = /([a-zA-Z])/;
+  var special_characters = /([~,!,@,#,$,%,^,&,*,-,_,+,=,?,>,<])/;
+  
+  //password_element = document.getElementById("password");
+  //console.log(password_element);
+  //let password = password_element.val().trim();
+	var password = $('#registration_form_password_first').val().trim();
+	if (password.length < 6) {
+		$('#password-strength-status').removeClass();
+    $('#password-strength-status').addClass('weak-password');
+    $('#registration_form_password_first').addClass('weak-password');
+    document.getElementById("progress-bar").style.width = "25%";
+    $('#progress-bar').removeClass();
+    $('#progress-bar').addClass('bg-red');
+		$('#password-strength-status').html("at least 6 characters.");
+	} else {
+		if (password.match(number) && password.match(alphabets) && password.match(special_characters)) {
+			$('#password-strength-status').removeClass();
+      $('#password-strength-status').addClass('strong-password');
+      $('#registration_form_password_first').addClass('strong-password');
+      document.getElementById("progress-bar").style.width = "100%";
+      $('#progress-bar').removeClass();
+      $('#progress-bar').addClass('bg-green');
+			$('#password-strength-status').html("Strong");
+		}
+		else {
+			$('#password-strength-status').removeClass();
+      $('#password-strength-status').addClass('medium-password');
+      $('#registration_form_password_first').addClass('medium-password');
+      document.getElementById("progress-bar").style.width = "50%";
+      $('#progress-bar').removeClass();
+      $('#progress-bar').addClass('bg-orange');
+			$('#password-strength-status').html("should include alphabets, numbers and special characters.");
+		}
+	}
+}
