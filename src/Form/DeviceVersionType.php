ec4b583cc5310ffb0b43f87be645feea3fc10da1
leaddn <leadieudonat@example.com>
2023-06-05 14:34:58 +0200

    updates

--- src/Form/DeviceVersionType.php
+++ src/Form/DeviceVersionType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Device;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -14,7 +15,7 @@ class DeviceVersionType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('versionUpload', TextType::class, [
+            ->add('versionUpload', TextareaType::class, [
                 'label' => false,
                 'attr' => [
                     'class' => 'flex-grow-1',
