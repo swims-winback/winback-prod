9144784d24cc3e0d3b397f246f6980cd3414a60e
leaddn <leadieudonat@gmail.com>
2023-08-16 12:34:21 +0200

    error logging mail

--- src/Command/TCPCommand.php
+++ src/Command/TCPCommand.php
@@ -8,6 +8,9 @@ use Ratchet\Server\IoServer;
 use App\Server\TCPServer;
 use Psr\Log\LoggerAwareInterface;
 use Psr\Log\LoggerInterface;
+use Symfony\Component\Console\ConsoleEvents;
+use Symfony\Component\Console\Event\ConsoleErrorEvent;
+use Symfony\Component\EventDispatcher\EventDispatcher;
 
 class TCPCommand extends Command
 {
@@ -30,6 +33,24 @@ class TCPCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $server = new TCPServer();
+        $pathToPython = "./src/Server/TCPServer.php";
+        //$server = new TCPServer(['php', $pathToPython]);
+        $dispatcher = new EventDispatcher();
+        $server->setDispatcher($dispatcher);
         $server->runServer($this->logger);
+
+        $dispatcher->addListener(ConsoleEvents::ERROR, function (ConsoleErrorEvent $event): void {
+            $output = $event->getOutput();
+        
+            $command = $event->getCommand();
+        
+            $output->writeln(sprintf('Oops, exception thrown while running command <info>%s</info>', $command->getName()));
+        
+            // gets the current exit code (the exception code)
+            $exitCode = $event->getExitCode();
+        
+            // changes the exception to another one
+            $event->setError(new \LogicException('Caught exception', $exitCode, $event->getError()));
+        });
     }
 }
