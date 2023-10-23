58bbb5a561165f46de66b2ae82ed975c412abfec
leaddn <leadieudonat@gmail.com>
2023-10-23 16:02:13 +0200

    new authentification

--- src/Entity/Main/ClientSn.php
+++ src/Entity/Main/ClientSn.php
@@ -6,6 +6,7 @@ use App\Repository\ClientSnRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: ClientSnRepository::class)]
+#[ORM\Table(name:"`client_sn`")]
 class ClientSn
 {
     #[ORM\Id]
